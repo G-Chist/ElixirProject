280748df37881e98bacf4b91f22669af5bf77607
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-21 16:52:16 -0400

    asdas

--- variables.exs
+++ variables.exs
@@ -2,6 +2,9 @@ gravity = 9.8  # Dynamic typing
 example_string = "This is a string!"
 example_bool = true
 example_atom = :true  # An atom's value is its own name (:true is also an atom)
+list_example = [1,2,3,4,5]  # Stored in memory as a linked list
 IO.puts gravity
 IO.puts example_string
 IO.puts example_bool == example_atom
+IO.puts List.first list_example
+IO.puts List.last list_example
