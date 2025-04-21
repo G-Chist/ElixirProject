5b82ce05bd7ce1fa4f9302ba1d52613c7b600ce6
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-21 16:05:54 -0400

    aasdas

--- simple_arithmetic.exs
+++ simple_arithmetic.exs
@@ -2,3 +2,6 @@ IO.puts 2+2  # Comments are just like in Python
 IO.puts 2*3
 IO.puts 2/3  # Automatic conversion to float
 IO.puts 3/2
+IO.puts div(200,15)  # For integer division (round down)
+IO.puts rem(200,15)  # Remainder
+IO.puts rem 200, 15  # Same as line 6
