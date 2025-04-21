bc54b3f9baa4e98badcecf57a706ad103ac97473
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-21 15:48:16 -0400

    asdas

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -2,4 +2,8 @@ defmodule Dictionary do
   def hello do
     IO.puts "Hello world!!"
   end
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list     = String.split(contents, ~r/\n/)  # Split by new line
+  end
 end
