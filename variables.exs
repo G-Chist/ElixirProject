51f49d817d582a4ecd4b920fb1c4134257871796
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-21 16:30:48 -0400

    asdas

--- variables.exs
+++ variables.exs
@@ -1,4 +1,7 @@
 gravity = 9.8  # Dynamic typing
 example_string = "This is a string!"
+example_bool = true
+example_atom = :true  # An atom's value is its own name
 IO.puts gravity
 IO.puts example_string
+IO.puts example_bool == example_atom
