080ec8f3aa4f45876105fe111699da489195facb
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-04-21 16:55:51 -0400

    asdas

--- variables.exs
+++ variables.exs
@@ -3,8 +3,10 @@ example_string = "This is a string!"
 example_bool = true
 example_atom = :true  # An atom's value is its own name (:true is also an atom)
 list_example = [1,2,3,4,5]  # Stored in memory as a linked list
+tuple_example = {1, 2, 3, 4, 5}  # Stored in memory as an array
 IO.puts gravity
 IO.puts example_string
 IO.puts example_bool == example_atom
-IO.puts List.first list_example
-IO.puts List.last list_example
+IO.puts List.first list_example  # Standard list method
+IO.puts List.last list_example  # Standard list method
+IO.puts elem(tuple_example, 0)  # Indices start with 0
