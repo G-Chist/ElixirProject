daeb29a62424d34d56ca654b234fd437242c7c51
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-05-02 17:34:29 -0400

    aasdas

---
+++ is_symmetric_binary_tree.exs
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+#
+# defmodule TreeNode do
+#   @type t :: %__MODULE__{
+#           val: integer,
+#           left: TreeNode.t() | nil,
+#           right: TreeNode.t() | nil
+#         }
+#   defstruct val: 0, left: nil, right: nil
+# end
+
+# From LeetCode
+
+defmodule Solution do
+  @spec is_symmetric(root :: TreeNode.t() | nil) :: boolean
+  def is_symmetric(root) do
+    is_mirror = fn is_mirror, tree1, tree2 ->
+      cond do  # Preferred to if - else if - else
+        tree1 == nil and tree2 == nil ->
+          true
+
+        tree1 == nil or tree2 == nil ->
+          false
+
+        tree1.val != tree2.val ->
+          false
+
+        true ->  # In any case, if not terminated
+          is_mirror.(is_mirror, tree1.left, tree2.right) and
+          is_mirror.(is_mirror, tree1.right, tree2.left)
+      end
+    end
+
+    is_mirror.(is_mirror, root, root)
+  end
+end
